3707d78ab7d99a5bb9bad9682a4894f98e238868
Raviteja466 <raviteja.ravi143@gmail.com>
2017-02-28 19:51:26 -0500

    working on React project commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RateCalculator from './components/rate-calculator';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+ReactDOM.render(
+  <RateCalculator />,
+  document.getElementById('root')
+);
